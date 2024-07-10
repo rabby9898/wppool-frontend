541f0f1c1d37e6e78bb8ca02d5f8ffff87f32535
rabby9898 <mdfajlerabby00@gmail.com>
2024-07-11 00:44:27 +0600

    banner design added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,9 +2,9 @@ import Banner from "../components/Banner/Banner";
 
 const Home = () => {
   return (
-    <>
+    <div>
       <Banner />
-    </>
+    </div>
   );
 };
 
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,29 @@
 import Navbar from "../navbar/Navbar";
-
+// import banner1 from "../../assets/banner-1.png";
+import "./Banner.css";
 const Banner = () => {
+  // bg-[#2042B6]
   return (
-    <div className="bg-[#2042B6] w-ful h-[718px]">
+    <div className="banner bg-[#2042B6] w-full h-[718px] z-50">
       <Navbar />
+      <div className="text-white mt-32">
+        <div className="my-16 px-8">
+          <h1 className="text-4xl">60</h1>
+          <p className="text-base">Companies</p>
+        </div>
+        <div className="my-16 px-8">
+          <h1 className="text-4xl">$100B</h1>
+          <p className="text-base">Market cap</p>
+        </div>
+        <div className="my-16 px-8">
+          <h1 className="text-4xl">3.5X</h1>
+          <p className="text-base">Revenue multiple</p>
+        </div>
+        <div className="my-16 px-8">
+          <h1 className="text-4xl">60%</h1>
+          <p className="text-base">LTM avg. revenue growth rate</p>
+        </div>
+      </div>
     </div>
   );
 };
