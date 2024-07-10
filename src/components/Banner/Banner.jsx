d00030905383eb86981b48668d03fd6f9e34bcbf
rabby9898 <mdfajlerabby00@gmail.com>
2024-07-10 18:34:53 +0600

    navbar design  added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import Navbar from "../navbar/Navbar";
 
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <div className="bg-[#2042B6] w-ful h-[718px]">
+      <Navbar />
+    </div>
+  );
 };
 
 export default Banner;
